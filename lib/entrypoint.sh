f763c5bb76e8dabfc961ed3e20f1e69ffaaa2d51
Taylor Thurlow <thurlow@hey.com>
2024-01-13 11:27:58 -0800

    Read standard version from bundle

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,6 +2,16 @@
 
 set -e
 
-gem install standard $ADDITIONAL_INSTALLED_GEMS
+if [ -n $STANDARD_VERSION ]; then
+	echo "STANDARD_VERSION set, installing specified version"
+	gem install standard -v "$STANDARD_VERSION"
+else
+	echo "STANDARD_VERSION not set, installing latest version"
+	gem install standard
+fi
+
+if [ -n $ADDITIONAL_INSTALLED_GEMS ]; then
+  gem install $ADDITIONAL_INSTALLED_GEMS
+fi
 
 ruby /action/lib/index.rb
